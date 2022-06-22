c040baf5e6285317c2cfa28c6fa9f4f3af12c4b0
Andrey Styskin <astyskin@gmail.com>
2022-06-22 23:24:53 +0300

    Fix pathes

--- download_models.sh
+++ download_models.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 mkdir -p models
-curl -L -O https://github.com/NyanNyanovich/nyan/releases/download/v0.1/nyan_models.tar.gz --output models/nyan_models.tar.gz
-cd models && tar -xzvf nyan_models.tar.gz && rm nyan_models.tar.gz
+cd models
+curl -L -O https://github.com/NyanNyanovich/nyan/releases/download/v0.1/nyan_models.tar.gz --output nyan_models.tar.gz
+tar -xzvf nyan_models.tar.gz && rm nyan_models.tar.gz
